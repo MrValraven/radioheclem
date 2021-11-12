7833c25e57adfc349fede3f87f648e78dd329733
mrvalraven <akatiago@gmail.com>
2021-11-12 20:58:25 +0000

    added new template message

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
+@import url("https://use.fontawesome.com/releases/v5.15.2/css/all.css");
+$specialColor: #155781;
+$radioPink: #e2669f;
+$radioLightGreen: #99f683;
+$radioGreen: #189d64;
+$radioBlue: #549eff;
+$radioOrange: #ff7733;
+$radioPurple: #544be2;
+$radioYellow: #f8d01c;
+$backgroundColor: #151415;
+$lighterBackgroundColor: #1e1e1f;
+$backgroundColorLight: #f7f8fc;
+
+@font-face {
+  font-family: "Metropolis";
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+  src: local("Metropolis"),
+    url("./assets/font/metropolis/Metropolis-Regular.otf") format("opentype");
+}
+@font-face {
+  font-family: "Metropolis";
+  font-weight: bold;
+  font-style: normal;
+  font-display: swap;
+  src: local("Metropolis"),
+    url("./assets/font/metropolis/Metropolis-Bold.otf") format("opentype");
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Metropolis", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $backgroundColorLight;
+  background-color: $backgroundColor;
+  cursor: default;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  &::-webkit-scrollbar {
+    width: 0.5rem;
   }
+  &::-webkit-scrollbar-track {
+    background-color: #f7f8fc;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #333333;
+    border-radius: 5px;
+  }
+}
+a {
+  text-decoration: none;
+  cursor: pointer;
+  opacity: 0.75;
+  transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  &:hover {
+    opacity: 1;
+  }
+}
+p {
+  hyphens: auto;
+}
+nav,
+ul,
+li {
+  list-style-type: none;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section :style="{ background: getImgURL('radio.jpg') }"></section>
+  <div class="card">
+    <h1>Radio Heclem</h1>
+    <p>Disponível em breve</p>
+    <div class="socials">
+      <i class="fab fa-instagram">
+        <a
+          href="https://www.instagram.com/radioheclem/"
+          target="_blank"
+          rel="noopenner noreferrer"
+        ></a>
+      </i>
+      <i class="fab fa-facebook-square">
+        <a
+          href="https://www.facebook.com/RadioHECLEM"
+          target="_blank"
+          rel="noopenner noreferrer"
+        ></a>
+      </i>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  methods: {
+    getImgURL(imageName) {
+      return require("@/assets/" + imageName);
+    },
+  },
+};
 </script>
+
+<style scoped lang="scss">
+section {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url("../assets/radio.jpg") no-repeat center center;
+  background-size: cover;
+  filter: blur(5px);
+}
+
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 400px;
+  height: 200px;
+  padding: 20px;
+  background: #1e1e1f;
+  box-shadow: 0 8px 32px 0 #1e1e1f;
+  backdrop-filter: blur(15.5px);
+  -webkit-backdrop-filter: blur(15.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+}
+</style>
